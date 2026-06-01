85bcefd2c8107f3dfb097458b135fbcfa1ef6ee6
olliethorner <olliethorner6@gmail.com>
2026-06-01 11:39:44 +0100

    Initial Commit

---
+++ PocketStampMerchant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketStampMerchant
+//
+//  Created by Ollie Thorner on 01/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketStampMerchant/PocketStampMerchantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketStampMerchantApp.swift
+//  PocketStampMerchant
+//
+//  Created by Ollie Thorner on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PocketStampMerchantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
